6f7eaa8ed9e70cdbcdcabe02d7a702d982ed5b33
Kane Zhu <kane.mx@gmail.com>
2020-01-06 17:57:40 +0800

    Capture the sumerian click event in AR page.

--- src/components/AR.js
+++ src/components/AR.js
@@ -29,8 +29,21 @@ class AR extends React.Component {
     window.location.href = "/";
   };
 
+  
+
   componentDidMount() {
     this.loadAndStartScene();
+    
+    var receiveMessage = function(event)
+    {
+      // Do we trust the sender of this message?  (might be
+      // different from what we originally opened, for example).
+      alert(event);
+  
+      // event.source is popup
+      // event.data is "hi there yourself!  the secret response is: rheeeeet!"
+    }
+    window.addEventListener("message", receiveMessage, false);
   }
 
   async loadAndStartScene() {
